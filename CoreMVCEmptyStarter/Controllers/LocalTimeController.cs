89c972399225d75673199d9b91c857b7591aff27
CW-ISM <cwismgitism@gmail.com>
2025-08-20 04:05:02 -0700

    Finished

---
+++ CoreMVCEmptyStarter/Controllers/LocalTimeController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Reflection.Metadata.Ecma335;
+
+namespace CoreMVCEmptyStarter.Controllers
+{
+    public class LocalTimeController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.CurrentTime = DateTime.Now;
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult GetCurrentTime()
+        {
+            string time = DateTime.Now.ToString();
+            return Content(time);
+        }
+    }
+}
---
+++ CoreMVCEmptyStarter/Controllers/RandomNumberController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CoreMVCEmptyStarter.Controllers
+{
+    public class RandomNumberController : Controller
+    {
+        public IActionResult Index()
+        {
+            Random random = new Random();
+            ViewBag.RandomNumber = random.Next(1, 100);
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult RandomNumber()
+        {
+            Random random = new Random();
+            string value = random.Next(1, 100).ToString();
+            return Content(value);
+        }
+    }
+}
